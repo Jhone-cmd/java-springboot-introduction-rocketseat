38c1ab58d95e9bab94f05bd063bb094915569e5a
Jhone-cmd <jhone.1941@gmail.com>
2026-01-27 14:51:42 -0300

    initial commit

---
+++ src/test/java/com/jhone_cmd/TodoList/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhone_cmd.TodoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
