97698c97014c59a46860b55f9f03f58722c2667e
Jhone-cmd <jhone.1941@gmail.com>
2026-01-29 15:59:25 -0300

    feat: setting attribute in request

--- src/main/java/com/jhone_cmd/TodoList/Filter/FilterTaskAuth.java
+++ src/main/java/com/jhone_cmd/TodoList/Filter/FilterTaskAuth.java
@@ -50,6 +50,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         } else {
             BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
             if (result.verified) {
+                request.setAttribute("idUser", user.getId());
                 filterChain.doFilter(request, response);
 
             } else {
--- src/main/java/com/jhone_cmd/TodoList/Filter/OncePerRequestFilter.java
+++
@@ -1,5 +0,0 @@
-package com.jhone_cmd.TodoList.Filter;
-
-public interface OncePerRequestFilter {
-
-}
