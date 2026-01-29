800b42fb5b9ffd12f1a0294e43a662583d32faba
Jhone-cmd <jhone.1941@gmail.com>
2026-01-29 15:10:52 -0300

    feat: task registration

---
+++ src/main/java/com/jhone_cmd/TodoList/tasks/ITaskRepository.java
@@ -0,0 +1,8 @@
+package com.jhone_cmd.TodoList.tasks;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+}
