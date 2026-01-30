74aa3477c295202c1a8ebce793c5c2d927b3ce09
Jhone-cmd <jhone.1941@gmail.com>
2026-01-30 15:36:07 -0300

    feat: hours validation

--- src/main/java/com/jhone_cmd/TodoList/tasks/TaskController.java
+++ src/main/java/com/jhone_cmd/TodoList/tasks/TaskController.java
@@ -1,8 +1,10 @@
 package com.jhone_cmd.TodoList.tasks;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +21,21 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity<Object> create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
         var task = taskRepository.save(taskModel);
-        return task;
+
+        var currentDate = LocalDateTime.now();
+
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(400).body("Task is not in the correct time range");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(400).body("Task start time must be before end time");
+        }
+
+        return ResponseEntity.status(201).body(task);
     }
 }
